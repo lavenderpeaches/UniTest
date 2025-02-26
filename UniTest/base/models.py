64fc831e08a61a4aa4d4cf457989c48ed9f96458
Deepali <122445268+lavenderpeaches@users.noreply.github.com>
2025-02-26 22:23:30 +0530

    Create models for classes -Batch -Course -Test

--- UniTest/base/models.py
+++ UniTest/base/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Batch(models.Model):
+    batch_name    = models.CharField(max_length = 255)
+    batch_section = models.CharField(max_length = 3)
+    batch_session = models.CharField(max_length = 20)
+    semester      = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.batch_name} | {self.batch_session} | {self.semester}th sem | {self.batch_section}"
+
+class Course(models.Model):
+    course_name = models.CharField(max_length = 255, unique = True)
+    course_code = models.CharField(max_length = 255, unique = True)
+
+    def __str__(self):
+        return f"{self.course_name} | {self.course_code}"
+
+class Test(models.Model):
+    name               = models.CharField(max_length = 255)
+    course             = models.ForeignKey(Course, on_delete = models.CASCADE, null=True, blank=True, default=None)
+    batch              = models.ForeignKey(Batch, on_delete = models.CASCADE)
+    total_marks        = models.IntegerField()
+    total_questions    = models.IntegerField()
+    duration           = models.DurationField()
+    is_results_visible = models.BooleanField(default = False)
+
+    def __str__(self):
+        return f"{self.name} | {self.course.course_name} |{self.batch.batch_name} | {self.total_marks} | {self.total_ques}"
+
+
+    
