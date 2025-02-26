f86468cf3e0c6aeceaf01983177af350331e995a
Deepali <122445268+lavenderpeaches@users.noreply.github.com>
2025-02-26 22:47:36 +0530

    Add the models to admin.py

--- UniTest/base/admin.py
+++ UniTest/base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Batch, Course, Test
 
-# Register your models here.
+# Register models
+admin.site.register(Batch)
+admin.site.register(Course)
+admin.site.register(Test)
